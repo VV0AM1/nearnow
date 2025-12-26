c679aec36cb7d7ce5892c8fca72e416884d9949b
ilia <serleb2000@gmail.com>
2025-12-26 16:03:17 +0100

    Phase 4 last fixes before deploy of the web version

--- apps/api/src/auth/dto/create-auth.input.ts
+++ apps/api/src/auth/dto/create-auth.input.ts
@@ -1,23 +1,30 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsString, IsOptional } from 'class-validator';
 
 @InputType()
 export class LoginInput {
   @Field()
+  @IsEmail()
   email: string;
 
   @Field()
+  @IsString()
   password: string;
 }
 
 @InputType()
 export class SignupInput {
   @Field()
+  @IsEmail()
   email: string;
 
   @Field()
+  @IsString()
   password: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   name?: string;
 }
 
